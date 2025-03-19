6b4a8fc2b94b9f7ce6e690e71ccec7d30acde284
Adrian Castilla <devops@customdevs.io>
2025-03-19 20:12:57 +0100

    ACC_Insercion metodos

--- src/main/java/com/mycompany/adrian_castilla_examendeentornos/Adrian_Castilla_ExamenDeEntornos.java
+++ src/main/java/com/mycompany/adrian_castilla_examendeentornos/Adrian_Castilla_ExamenDeEntornos.java
@@ -9,8 +9,44 @@ package com.mycompany.adrian_castilla_examendeentornos;
  * @author adriancastilla
  */
 public class Adrian_Castilla_ExamenDeEntornos {
-
+    
+    public static double calculoAreaRectangulo(ACC_Rectangulo rectangulo1) {
+        double ladoA = rectangulo1.getLadoA();
+        double ladoB = rectangulo1.getLadoB();
+        double area1 = ladoA * ladoB;
+        return area1;
+    }
+    
+    public static double calculoAreaCirculo(ACC_Circular circulo1) {
+        double radio1 = circulo1.getRadio();
+        double area2 = radio1 * 3.14;
+        return area2;
+    }
+    
+    public static double calculoDiferencia(double area1, double area2) {
+        double diferencia = area1 - area2;
+        return diferencia;
+    }
+    
+    public static void printValues(double areaRectangulo, double areaCirculo, double diferencia) {
+        System.out.println("Area cuadrado: " + areaRectangulo);
+        
+        System.out.println("Area circulo: " + areaCirculo);
+        
+        System.out.println("Diferencia areas: " + diferencia);
+    }
+    
+    
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+           ACC_Rectangulo rectangulo1 = new ACC_Rectangulo(3, 3, 4, "verde");
+           ACC_Circular circulo1 = new ACC_Circular(3, 3, "naranja");
+           
+           double areaRectangulo = rectangulo1.calculoAreaRectangulo();
+           double areaCirculo = circulo1.calculoAreaCirculo();
+           double diferencia = calculoDiferencia(areaRectangulo, areaCirculo);
+           
+           printValues(areaRectangulo, areaCirculo, diferencia);
+        
     }
 }
