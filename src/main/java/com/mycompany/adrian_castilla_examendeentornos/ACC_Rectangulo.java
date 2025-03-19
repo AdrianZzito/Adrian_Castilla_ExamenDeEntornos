79df531ef618aaa17e5864f67af32ec9b7601a40
Adrian Castilla <devops@customdevs.io>
2025-03-19 19:27:49 +0100

    ACC_Commit de la segunda clase cuadrado

---
+++ src/main/java/com/mycompany/adrian_castilla_examendeentornos/ACC_Rectangulo.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.adrian_castilla_examendeentornos;
+
+/**
+ *
+ * @author adriancastilla
+ */
+public class ACC_Rectangulo {
+    int id;
+    double ladoA;
+    double ladoB;
+    String color;
+
+    public ACC_Rectangulo(int id, double ladoA, double ladoB, String color) {
+        this.id = id;
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getLadoA() {
+        return ladoA;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLadoA(double ladoA) {
+        this.ladoA = ladoA;
+    }
+
+    public void setLadoB(double ladoB) {
+        this.ladoB = ladoB;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "ACC_Rectangulo{" + "id=" + id + ", ladoA=" + ladoA + ", ladoB=" + ladoB + ", color=" + color + '}';
+    }
+        
+    
+    
+}
